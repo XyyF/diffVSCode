9b65c4642f097149778a3f67eea50a9e0f47bcc9
rengarxiao <rengarxiao@tencent.com>
2020-11-07 10:40:24 +0800

    feat: 重命名

--- src/extension.ts
+++ src/extension.ts
@@ -13,7 +13,7 @@ export function activate(context: vscode.ExtensionContext) {
 	console.log('Congratulations, your extension "diffvscode" is now active!');
 
 	// 注册命令
-	let disposable = vscode.commands.registerCommand('diffvscode.sayHello', () => {
+	let disposable = vscode.commands.registerCommand('diffvscode.retrofitJs', () => {
 		// The code you place here will be executed every time your command is executed
 
 		// Display a message box to the user
