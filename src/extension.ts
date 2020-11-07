6291870bdfafb3d785cda3053ecebe883e7bf001
rengarxiao <rengarxiao@tencent.com>
2020-11-07 10:34:49 +0800

    feat: init

--- src/extension.ts
+++ src/extension.ts
@@ -2,26 +2,30 @@
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
+/**
+ * 插件被激活时触发，所有代码总入口
+ * @param context 插件上下文
+ */
 export function activate(context: vscode.ExtensionContext) {
 
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "diffvscode" is now active!');
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('diffvscode.helloWorld', () => {
+	// 注册命令
+	let disposable = vscode.commands.registerCommand('diffvscode.sayHello', () => {
 		// The code you place here will be executed every time your command is executed
 
 		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from diffVSCode!');
+		vscode.window.showInformationMessage('Hello World from diffVSCoddawde!');
 	});
 
 	context.subscriptions.push(disposable);
 }
 
-// this method is called when your extension is deactivated
-export function deactivate() {}
+/**
+ * 插件被释放时触发
+ */
+export function deactivate() {
+	console.log('您的扩展已被释放！')
+}
