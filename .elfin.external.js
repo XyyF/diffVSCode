ef33f032576f031bc72d11d726cb992772fbd851
rengarxiao <rengarxiao@tencent.com>
2020-11-27 14:24:29 +0800

    Revert "test"
    This reverts commit f275fd273de8fb589175be4877a321bdd5ca15ad.

--- .elfin.external.js
+++
@@ -1,7 +0,0 @@
-module.exports = [
-    {
-        sshAddress: 'git@github.com:XyyF/elfin-eslint.git',                   /* git ssh地址 */
-        branch: 'main',              /* clone的分支，默认master */
-        skipInstall: false,             /* 是否跳过install依赖，默认为false */
-    },
-]
