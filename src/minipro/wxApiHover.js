4dd5636ef1f94bc6a800a4e2c9f35afdac02bc16
rengarxiao <rengarxiao@tencent.com>
2021-02-09 09:50:51 +0800

    feat: js

--- src/minipro/wxApiHover.js
+++ src/minipro/wxApiHover.js
@@ -6,7 +6,7 @@ const documents = {};
 const shell = new Shell();
 
 // 遍历所有的json
-// const miniproJs = JSON.parse(shell.loadFileFromElfinRoot('src/snippets/miniproJs.json'));
+const miniproJs = JSON.parse(shell.loadFileFromElfinRoot('src/snippets/miniproJs.json'));
 const miniproJson = JSON.parse(shell.loadFileFromElfinRoot('src/snippets/miniproJson.json'));
 const miniproWxml = JSON.parse(shell.loadFileFromElfinRoot('src/snippets/miniproWxml.json'));
 // 遍历所有的markdown
@@ -16,7 +16,23 @@ markdowns.forEach((file) => {
   documents[filename] = shell.loadFlieFromScript(file);
 });
 
-function provideHover(document, position) {
+function provideHoverJs(document, position) {
+  let apiName = document.getText(document.getWordRangeAtPosition(position));
+  if (!apiName) return;
+
+  apiName = apiName.replace(/"/g, '');
+  if (documents[apiName]) {
+    return new vscode.Hover(new vscode.MarkdownString(documents[apiName]));
+  }
+  const jstag = `minipro-js-${apiName}`;
+  if (miniproJs[jstag]) {
+    return new vscode.Hover(
+      new vscode.MarkdownString(`${miniproJs[jstag].description} \r\n` + '\r\n' + `[官方文档](${miniproJs[jstag].url})`)
+    );
+  }
+  return null;
+}
+function provideHoverWxml(document, position) {
   let apiName = document.getText(document.getWordRangeAtPosition(position));
   if (!apiName) return;
 
@@ -30,6 +46,16 @@ function provideHover(document, position) {
       new vscode.MarkdownString(`${miniproWxml[wxmltag].description} \r\n` + '\r\n' + `[官方文档](${miniproWxml[wxmltag].url})`)
     );
   }
+  return null;
+}
+function provideHoverJson(document, position) {
+  let apiName = document.getText(document.getWordRangeAtPosition(position));
+  if (!apiName) return;
+
+  apiName = apiName.replace(/"/g, '');
+  if (documents[apiName]) {
+    return new vscode.Hover(new vscode.MarkdownString(documents[apiName]));
+  }
   const jsontag = `minipro-json-${apiName}`;
   if (miniproJson[jsontag]) {
     return new vscode.Hover(
@@ -42,8 +68,14 @@ function provideHover(document, position) {
 module.exports = function (context) {
   // 注册实现定义提示，第一个参数表示仅对javascript文件生效
   context.subscriptions.push(
-    vscode.languages.registerHoverProvider(['javascript', 'wxml', 'json'], {
-      provideHover,
-    })
+    vscode.languages.registerHoverProvider(['javascript'], {
+      provideHover: provideHoverJs,
+    }),
+    vscode.languages.registerHoverProvider(['wxml'], {
+      provideHover: provideHoverWxml,
+    }),
+    vscode.languages.registerHoverProvider(['json'], {
+      provideHover: provideHoverJson,
+    }),
   );
 };
